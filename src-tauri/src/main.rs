c66708a9cc98ca5290328e85cf9e007138307ae1
tdelestr <thomas.delestre.pro@gmail.com>
2024-12-17 17:57:08 +0100

    feat front-back req/resp ok

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,6 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-// Define the function you want to call from React
-#[tauri::command]
-fn greet(name: &str) -> String {
-    println!("coucou");
-    format!("Hello, {}!", name)
-}
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "console")]
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    exchange_dashboard_lib::run()
 }
